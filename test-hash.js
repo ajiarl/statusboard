47b590feb99d9c2d2a4ca67403204fca81b71e20
ajiarl <ajiarlando127@gmail.com>
2026-08-20 04:22:56 +0700

    chore: remove test-hash.js

--- test-hash.js
+++
@@ -1,9 +0,0 @@
-﻿const bcrypt = require('bcryptjs');
-const password = 'password';
-const hash = process.argv[2];
-
-bcrypt.compare(password, hash).then(result => {
-  console.log('Password:', password);
-  console.log('Hash:', hash);
-  console.log('Match:', result);
-});
